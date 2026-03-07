be8c88a8898a8761c36ee0002556da6b62fa9613
thailisg <thai.ori.gon.diaz@gmail.com>
2026-03-07 13:45:04 -0300

    correction in product.js about the localStorage

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -8,20 +8,9 @@ console.log("Product ID from URL:", productId);
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-	if (!product) {
-		console.error("Cannot add null product to cart");
-		return;
-	}
-	let cartItems = getLocalStorage("so-cart");
-	// Ensure cartItems is always an array
-	if (!Array.isArray(cartItems)) {
-		cartItems = [];
-	}
-	cartItems.push(product);
-	setLocalStorage("so-cart", cartItems);
-	console.log("Product added to cart:", product);
-	console.log("Cart now has", cartItems.length, "items");
-	alert(`${product.Name} added to cart!`);
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {

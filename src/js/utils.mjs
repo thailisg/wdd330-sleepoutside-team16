3058f5f4dd8ad6519431ea389ee1f1d160024608
thailisg <thai.ori.gon.diaz@gmail.com>
2026-03-14 20:16:16 -0300

    my individual activity w02

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,3 +27,18 @@ export function getParam(param) {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  const htmlStrings = list.map(templateFn).join("");
+  parentElement.insertAdjacentHTML(position, htmlStrings);
+}

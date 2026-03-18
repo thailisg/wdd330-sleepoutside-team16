475930efbbff2be42665ac283a08300d927a765a
thailis gonzalez <115045809+thailisg@users.noreply.github.com>
2026-03-17 21:59:59 -0300

    Update utils.mjs
    arranging code for the render

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -32,6 +32,12 @@ export function setClick(selector, callback) {
 	qs(selector).addEventListener("click", callback);
 }
 
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
 export function renderListWithTemplate(
 	templateFn,
 	parentElement,

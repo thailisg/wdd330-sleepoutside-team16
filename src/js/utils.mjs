04e60d2471284f3c8606d8bbe0eaf89094ae87d9
thailisg <thai.ori.gon.diaz@gmail.com>
2026-03-23 16:55:09 -0300

    Correct some errors to be able to display stores in product listings

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -33,9 +33,9 @@ export function setClick(selector, callback) {
 }
 
 export function getParam(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(param);
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	return urlParams.get(param);
 }
 
 export function renderListWithTemplate(
@@ -54,31 +54,30 @@ export function renderListWithTemplate(
 }
 
 export function renderWithTemplate(
-  template,
-  parentElement,
-  data,
-  callback
+	template,
+	parentElement,
+	data,
+	callback
 ) {
-  parentElement.innerHTML = template;
-  if (callback) {
-    callback(data);
-  }
+	parentElement.innerHTML = template;
+	if (callback) {
+		callback(data);
+	}
 }
 
 export async function loadTemplate(path) {
-  const res = await fetch(path);
-  const template = await res.text();
-  return template;
+	const res = await fetch(path);
+	const template = await res.text();
+	return template;
 }
 
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("../partials/header.html");
-  const footerTemplate = await loadTemplate("../partials/footer.html");
+	const headerTemplate = await loadTemplate("../partials/header.html");
+	const footerTemplate = await loadTemplate("../partials/footer.html");
 
-  const headerElement = document.querySelector("#main-header");
-  const footerElement = document.querySelector("#main-footer");
+	const headerElement = document.querySelector("#main-header");
+	const footerElement = document.querySelector("#main-footer");
 
-  renderWithTemplate(headerTemplate, headerElement);
-  renderWithTemplate(footerTemplate, footerElement);
+	renderWithTemplate(headerTemplate, headerElement);
+	renderWithTemplate(footerTemplate, footerElement);
 }
-

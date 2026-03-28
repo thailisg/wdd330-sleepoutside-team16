0ec58d048ad38d4c7806bc91b36e414ac3d25aa0
thailisg <thai.ori.gon.diaz@gmail.com>
2026-03-28 17:32:15 -0300

    W04 theme update

--- src/checkout/checkout.js
+++ src/checkout/checkout.js
@@ -1,5 +1,17 @@
 import { loadHeaderFooter } from "../js/utils.mjs";
+import CheckoutProcess from "../js/CheckoutProcess.mjs";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await loadHeaderFooter();
+
+  const checkout = new CheckoutProcess("so-cart", "#order-summary");
+
+  checkout.init();
+  checkout.calculateItemSubTotal();
+
+  const zipInput = document.getElementById("zip");
+
+  zipInput.addEventListener("change", () => {
+    checkout.calculateOrderTotal();
+  });
 });
--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,23 +1,36 @@
-import { calculateTotal } from "./cart.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
 export default class CheckoutProcess {
-    constructor() {
-        this.subtotal = 0;
-        this.tax = 0;
+
+    constructor(key, outputSelector) {
+        this.key = key;
+        this.outputSelector = outputSelector;
+        this.list = [];
+        this.itemTotal = 0;
         this.shipping = 0;
-        this.total = 0;
+        this.tax = 0;
+        this.orderTotal = 0;
     }
 
-    calculateSubtotal(totalCart) {
-        this.subtotal = calculateTotal(totalCart);
+    init() {
+        this.list = getLocalStorage(this.key) || [];
     }
 
-    calculateTax() {
-        this.tax = this.subtotal * 0.06;
+    // Subtotal
+    calculateItemSubTotal() {
+        this.itemTotal = this.list.reduce(
+            (sum, item) => sum + Number(item.FinalPrice),
+            0
+        );
     }
 
-    calculateShipping(items) {
-        const totalItems = cartItems.reduce((sum, item) => sum + (item.quantity || 1), 0);
+    // Shipping
+    calculateShipping() {
+        const totalItems = this.list.reduce(
+            (sum, item) => sum + (item.quantity || 1),
+            0
+        );
+
         if (totalItems === 0) {
             this.shipping = 0;
         } else {
@@ -25,14 +38,29 @@ export default class CheckoutProcess {
         }
     }
 
-    calculateTotal() {
-        this.total = this.subtotal + this.tax + this.shipping;
+    // All total
+    calculateOrderTotal() {
+        this.tax = this.itemTotal * 0.06;
+
+        this.calculateShipping();
+
+        this.orderTotal = this.itemTotal + this.tax + this.shipping;
+
+        this.displayOrderTotals();
     }
 
-    updateTotals(cartItems) {
-        this.calculateSubtotal(cartItems);
-        this.calculateTax();
-        this.calculateShipping(cartItems);
-        this.calculateTotal();
+    // display in checkout index
+    displayOrderTotals() {
+        document.querySelector("#subtotal-value").innerText =
+            `$${this.itemTotal.toFixed(2)}`;
+
+        document.querySelector("#tax-value").innerText =
+            `$${this.tax.toFixed(2)}`;
+
+        document.querySelector("#shipping-value").innerText =
+            `$${this.shipping.toFixed(2)}`;
+
+        document.querySelector("#total-value").innerText =
+            `$${this.orderTotal.toFixed(2)}`;
     }
 }
